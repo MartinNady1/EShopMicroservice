094ed4a5a59318cc077ce4744ba417e4b3780385
Martin <martinnady333@gmail.com>
2026-02-20 07:25:27 +0200

    YARP

---
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddRateLimiter(options => {
+    options.AddFixedWindowLimiter("fixedWindowLimiter", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.UseRateLimiter();
+app.MapReverseProxy();
+app.Run();
